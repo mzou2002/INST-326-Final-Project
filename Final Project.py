53b4a4c14e24cd0a40b9069efd744a5346587c4a
mzou2002 <matthewzou707@gmail.com>
2024-11-12 17:05:10 -0500

    upload docstring

--- Final Project.py
+++ Final Project.py
@@ -1,1 +1,84 @@
-HI
+import re
+import pandas as pd
+import csv
+
+Class Search:
+    def __init__ ():
+
+"""
+Genre
+Searches movies by genre
+Args:
+Data - movie data csv file
+
+Returns:
+List of movies with matching genre
+"""
+
+“““Year
+Searches movie by the year.
+
+Args:
+Data - movie data csv file
+
+Returns:
+List of movies with matching year
+”””
+
+“““Movie_rating
+Data: movie data csv file
+Args:
+Data - movie data csv file
+
+Returns:
+List of movies with matching rating
+”””
+
+“““Longest_Movies
+Returns list of 10 longest movies in terms of duration
+Data: movie data csv file
+
+Args:
+Data - movie data csv file
+
+Returns:
+List of movies
+”””
+
+““Shortest_Movies
+Returns list of 10 shortest movies in terms of duration
+Data: movie data csv file
+
+Args:
+Data - movie data csv file
+
+Returns:
+List of movies
+”””
+
+
+“““Movie_name
+Data: movie data csv file
+Returns: Information about movie with matching name
+”””
+
+“““Content_rating
+What age rating is the movie. For example, PG-13, R, etc.
+Args:
+Data - csv file with movie data
+
+Returns:
+Movie with 
+”””
+
+“““Movie_recommendation
+Return a short list of movies that we recommend
+
+Args:
+Data - csv file
+
+Return:
+List of movies we recommend
+”””
+
+Main
