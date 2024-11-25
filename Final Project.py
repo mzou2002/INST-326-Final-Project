f98ec12282f690172e1b915a3c52aab6c5ed5e3f
mzou2002 <matthewzou707@gmail.com>
2024-11-25 00:00:53 -0500

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -1,84 +1,91 @@
 import re
 import pandas as pd
 import csv
+import sys
+import argparse
 
-Class Search:
-    def __init__ ():
+class Search:
+    def __init__ (self, data, keyword):
+        pass
 
-"""
-Genre
-Searches movies by genre
-Args:
-Data - movie data csv file
+    def genre(self):
+        """
+        Searches movies by genre
+        
+        Args:
+        Data: movie data csv file
+        
+        Return:
+        List of movies with matching genre
+        """
 
-Returns:
-List of movies with matching genre
-"""
+    def year(self):
+        pass
 
-“““Year
-Searches movie by the year.
+    """Year
+    Searches movie by the year.
 
-Args:
-Data - movie data csv file
+    Args:
+    Data - movie data csv file
 
-Returns:
-List of movies with matching year
-”””
+    Returns:
+    List of movies with matching year
+    """
 
-“““Movie_rating
-Data: movie data csv file
-Args:
-Data - movie data csv file
+    def movie_rating(self):
+        pass
+    """Movie_rating
+    Data: movie data csv file
+    Args:
+    Data - movie data csv file
 
-Returns:
-List of movies with matching rating
-”””
+    Returns:
+    List of movies with matching rating
+    """
 
-“““Longest_Movies
-Returns list of 10 longest movies in terms of duration
-Data: movie data csv file
+    def movie_duration(self, keyword):
+        pass
 
-Args:
-Data - movie data csv file
+    """
+    Returns list of 10 longest or shortest movies in terms of duration
+    Data: movie data csv file
 
-Returns:
-List of movies
-”””
+    Args:
+    Data - movie data csv file
 
-““Shortest_Movies
-Returns list of 10 shortest movies in terms of duration
-Data: movie data csv file
+    Returns:
+    List of movies
+    """
 
-Args:
-Data - movie data csv file
-
-Returns:
-List of movies
-”””
+    def movie_name(self):
+        pass
 
 
-“““Movie_name
-Data: movie data csv file
-Returns: Information about movie with matching name
-”””
+    """Movie_name
+    Data: movie data csv file
+    Returns: Information about movie with matching name
+    """
 
-“““Content_rating
-What age rating is the movie. For example, PG-13, R, etc.
-Args:
-Data - csv file with movie data
+    def content_rating(self):
+        pass
+    """Content_rating
+    What age rating is the movie. For example, PG-13, R, etc.
+    Args:
+    Data - csv file with movie data
 
-Returns:
-Movie with 
-”””
+    Returns:
+    Movie with 
+    """
 
-“““Movie_recommendation
-Return a short list of movies that we recommend
+    def recommendation(self):
+        pass
+    """Movie_recommendation
+    Return a short list of movies that we recommend
 
-Args:
-Data - csv file
+    Args:
+    Data - csv file
 
-Return:
-List of movies we recommend
-”””
+    Return:
+    List of movies we recommend
+    """
 
-Main
